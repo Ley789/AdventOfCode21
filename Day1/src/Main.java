47acb159f5a28e6a9b2b6ee1455e6ce31c9d8fed
Alexander lochmann <alex_lochmann@hotmail.de>
2022-11-15 00:34:19 +0100

    added solution to second exercise

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -26,20 +26,38 @@ public class Main {
 
 	public static long CountIncrements(List<Integer> in) {
 		if (in.size() <= 1) return 0;
-	    //We know that in.size() > 2, so we can start count increments
+	    //We know that in.size() >= 2, so we can start count increments
 		long count = 0;
-		long compare = in.get(0);
+		long comparePre = in.get(0);
+		long compareAft;
 		for (int i = 1; i < in.size(); i++) {
-			if (compare < in.get(i)) count++;
-			compare = in.get(i);
+			compareAft = in.get(i);
+			if (comparePre < compareAft) count++;
+			comparePre = compareAft;
+		}
+		return count;
+	}
+	
+	public static long CountThreeIncrements(List<Integer> in) {
+		if (in.size() <= 2) return 0;
+	    //We know that in.size() >= 3, so we can start count increments
+		long count = 0;
+		long comparePre = in.get(0) + in.get(1) + in.get(2);
+		long compareAft = 0;
+		for (int i = 3; i < in.size(); i++) {
+			compareAft = in.get(i - 2) + in.get(i - 1) + in.get(i);
+			if (comparePre < compareAft) count++;
+			comparePre = compareAft;
 		}
 		return count;
 	}
 	
 	public static void main(String[] args) {
 		List<Integer> input = ReadProblem("InputFile.txt");
-		long solution = CountIncrements(input);
-		System.out.println(solution);
+		long solutionDay1 = CountIncrements(input);
+		long solutionDay2 = CountThreeIncrements(input);
+		System.out.println(solutionDay1);
+		System.out.println(solutionDay2);
 	}
 
 }
