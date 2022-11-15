6a69e245a9c3c62ea13d21cde0536f41ab4e9511
Alexander lochmann <alex_lochmann@hotmail.de>
2022-11-15 12:23:16 +0100

    redesigned first part and completet second part of day 2 task

--- Day2/src/Main.java
+++ Day2/src/Main.java
@@ -7,46 +7,107 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+	enum Command{
+		F,
+		D,
+		U
+	}
 	//vPosOffset checks how deep we are, so it cannot be negative
-	public static class Position {
-		public long hPosOffset = 0;
-		public long vPosOffset = 0;
-		public Position() {};
+	public static class SubmarinC {
+		public long magnitute = 0;
+		public Command type;
+		public SubmarinC() {};
 		public String toString() {
-			return "horizontal offset change is " + hPosOffset + " and vertical offset change is " + vPosOffset;
+			return "offset change is " + magnitute + " and the tag is " + type;
 			
-		}
-		public void addHorizontalOffset(long delta) {
-			this.hPosOffset += delta;
-		}
-		
-		public void addVerticalOffset(long delta) {
-			long res = vPosOffset + delta;
-			if (res <= 0) res = 0;
-			this.vPosOffset = res;
-		}
-		
-		public void addPosition(Position other) {
-			this.addHorizontalOffset(other.hPosOffset);
-			this.addVerticalOffset(other.vPosOffset);
 		}
 	}
 	
+	public static class Position {
+		long hPos = 0;
+		long vPos = 0;
+	}
+	
+	public static class Submarine{
+		public Position p;
+		public int aim = 0;
+		Submarine (){
+			p = new Position();
+		}
+		Submarine (Position initPos){
+			p = initPos;
+		}
+		
+		//Task 1; ignoring aim
+		public void SubmarinCommanTask1(SubmarinC c) {
+			switch(c.type) {
+			case F:
+				p.hPos += c.magnitute;
+				break;
+			case D:
+				p.vPos += c.magnitute;
+				break;
+			case U:
+				p.vPos -= c.magnitute;
+				//Remember we are a submarine, we cannot be higher then 0
+				if (p.vPos < 0) p.vPos = 0;
+				break;
+			}
+		}
+		
+		//Task 2; works with aim
+		public void SubmarinCommanTask2(SubmarinC c) {
+			switch(c.type) {
+			case F:
+				p.hPos += c.magnitute;
+				p.vPos += aim * c.magnitute;
+				if (p.vPos < 0) p.vPos = 0;
+				break;
+			case D:
+				aim += c.magnitute;
+				break;
+			case U:
+				aim -= c.magnitute;
+				break;
+			}
+		}
+	}
+
+
+	
+
+	public static void main(String[] args) {
+		List<SubmarinC> input = ReadProblem("InputFile.txt");
+        Submarine driveTask1 = new Submarine();
+        Submarine driveTask2 = new Submarine();
+		for (SubmarinC command : input) 
+		{ 
+			driveTask1.SubmarinCommanTask1(command);
+			driveTask2.SubmarinCommanTask2(command);
+		}
+		System.out.println(driveTask1.p.hPos * driveTask1.p.vPos);
+		System.out.println(driveTask2.p.hPos * driveTask2.p.vPos);
+	}
+	
+	
+	
+	
 	//convention filename is in the same folder as the executable
-	public static List<Position> ReadProblem (String filename){
+	public static List<SubmarinC> ReadProblem (String filename){
         Path p = Paths.get(System.getProperty("user.dir"));
         p = p.resolve(filename);
-        List<Position> res = new ArrayList<>();
+        List<SubmarinC> res = new ArrayList<>();
         File f = p.toFile();
 	    f.setReadOnly();
         try (Scanner scan = new Scanner (f)) {
 		    while (scan.hasNextLine()) {
 			    String[] r = scan.nextLine().split(" ");
 			    if (r.length != 2) { throw new RuntimeException("Input file not correct"); }
-			    Position pos = new Position();
-			    if (r[0].matches("forward"))   pos.hPosOffset = Long.parseLong(r[1]);
-			    else if (r[0].matches("up"))   pos.vPosOffset = -Long.parseLong(r[1]);
-			    else if (r[0].matches("down")) pos.vPosOffset = Long.parseLong(r[1]);
+			    SubmarinC pos = new SubmarinC();
+		    	pos.magnitute = Long.parseLong(r[1]);
+			    if (r[0].matches("forward"))   pos.type = Command.F;
+			    else if (r[0].matches("up"))   pos.type = Command.U;
+			    else if (r[0].matches("down")) pos.type = Command.D;
 			    else { throw new RuntimeException("Input file not correct"); }
 	            res.add(pos);
 			}
@@ -54,19 +115,6 @@ public class Main {
         	System.out.println("Read conventions bro");
         }
       return res;
-	}
-
-
-	public static void main(String[] args) {
-		List<Position> input = ReadProblem("InputFile.txt");
-		Position endPos = new Position();
-		for (Position p : input) 
-		{ 
-		   endPos.addPosition(p);
-		}
-		System.out.println(endPos);
-		System.out.println(endPos.hPosOffset * endPos.vPosOffset);
-		
 	}
 
 }
